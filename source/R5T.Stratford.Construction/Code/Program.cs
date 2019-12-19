6754f13432c3eea1aa449f8e1dda689d3d99beaa
David Coats <david@anyoneone.com>
2019-12-18 20:57:24 -0800

    Added utility methods to find types inheriting from the IServiceDefinition interface and to find types with the ServiceDefinitionAttribute.

--- source/R5T.Stratford.Construction/Code/Program.cs
+++ source/R5T.Stratford.Construction/Code/Program.cs
@@ -19,36 +19,34 @@ namespace R5T.Stratford.Construction
             // Get all assembly types.
             var assemblyTypes = assembly.GetTypes();
 
+            Console.WriteLine($"Assembly {assembly.FullName} - All types:");
+
             foreach (var type in assemblyTypes)
             {
                 Console.WriteLine(type.Name);
             }
 
             // Get types inheriting (that can be assigned to) the service definition interface.
-            var serviceDefinitionInterfaceType = typeof(IServiceDefinition);
+            var serviceDefinitionTypes = Utilities.FindIServiceDefinitionInterfaceTypes(assemblyTypes);
 
-            var serviceDefinitionTypes = assemblyTypes.Where(type => type.IsInterface && serviceDefinitionInterfaceType.IsAssignableFrom(type));
+            Console.WriteLine();
+            Console.WriteLine($"{typeof(IServiceDefinition).Name} types:");
 
             foreach (var serviceDefinitionType in serviceDefinitionTypes)
             {
-                Console.WriteLine($"{serviceDefinitionType.Name}: {serviceDefinitionInterfaceType.Name}");
+                Console.WriteLine($"{serviceDefinitionType.Name}");
             }
             // Note: with the IsInterface check, all interfaces that inherit from an interface that inherits from the service definition interface will also inherit from the service definition interface.
 
             // Get types with the service definition attribute.
-            var serviceDefinitionAttribute = typeof(ServiceDefinitionAttribute);
+            var serviceDefinitionAttributedTypes = Utilities.FindServiceDefinitionAttributeTypes(assemblyTypes);
 
-            var serviceDefinitionAttributedTypes = assemblyTypes.Where(type =>
-            {
-                var customAttributes = type.GetCustomAttributes(serviceDefinitionAttribute, true); // Make sure to include inheritance tree.
-
-                var hasAttribute = customAttributes.Count() > 0;
-                return hasAttribute;
-            });
+            Console.WriteLine();
+            Console.WriteLine($"{typeof(ServiceDefinitionAttribute).Name} - Has attribute:");
 
             foreach (var serviceDefinitionAttributedType in serviceDefinitionAttributedTypes)
             {
-                Console.WriteLine($"{serviceDefinitionAttributedType.Name}: has attribute {serviceDefinitionAttribute.Name}");
+                Console.WriteLine($"{serviceDefinitionAttributedType.Name}");
             }
             // Note: only the attributed interface is found, not classes implementing the interface, since the attribute only applies to interfaces.
         }
---
+++ source/R5T.Stratford/Code/Utilities.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+
+namespace R5T.Stratford
+{
+    public static class Utilities
+    {
+        /// <summary>
+        /// Finds types that are interfaces and are assignable to <see cref="IServiceDefinition"/>.
+        /// </summary>
+        public static IEnumerable<Type> FindIServiceDefinitionInterfaceTypes(IEnumerable<Type> types)
+        {
+            var serviceDefinitionInterfaceType = typeof(IServiceDefinition);
+
+            var serviceDefinitionTypes = types.Where(type => type.IsInterface && serviceDefinitionInterfaceType.IsAssignableFrom(type));
+            return serviceDefinitionTypes;
+        }
+
+        public static IEnumerable<Type> FindIServiceDefinitionInterfaceTypes(Assembly assembly)
+        {
+            var assemblyTypes = assembly.GetTypes();
+
+            var serviceDefinitionTypes = Utilities.FindIServiceDefinitionInterfaceTypes(assemblyTypes);
+            return serviceDefinitionTypes;
+        }
+
+        public static IEnumerable<Type> FindServiceDefinitionAttributeTypes(IEnumerable<Type> types)
+        {
+            var serviceDefinitionAttribute = typeof(ServiceDefinitionAttribute);
+
+            var serviceDefinitionAttributedTypes = types.Where(type =>
+            {
+                var customAttributes = type.GetCustomAttributes(serviceDefinitionAttribute, true); // Make sure to include inheritance tree.
+
+                var hasAttribute = customAttributes.Count() > 0;
+                return hasAttribute;
+            });
+
+            return serviceDefinitionAttributedTypes;
+        }
+
+        public static IEnumerable<Type> FindServiceDefinitionAttributeTypes(Assembly assembly)
+        {
+            var assemblyTypes = assembly.GetTypes();
+
+            var serviceDefinitionTypes = Utilities.FindServiceDefinitionAttributeTypes(assemblyTypes);
+            return serviceDefinitionTypes;
+        }
+    }
+}
