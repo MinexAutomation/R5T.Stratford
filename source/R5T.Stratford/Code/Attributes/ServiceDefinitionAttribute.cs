0370612343f9ce6dba89f5ad03033bdeb8d5247c
David Coats <david@anyoneone.com>
2019-12-18 20:47:22 -0800

    Added service definition interface and attribute.

---
+++ source/R5T.Stratford/Code/Attributes/ServiceDefinitionAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace R5T.Stratford
+{
+    /// <summary>
+    /// Attribute indicating that the attributed interface is a service definition.
+    /// Inherited is false, since classes *implement* an interface, not inherit from it, and inherited interfaces also do not seem to work.
+    /// Note that class which implement the attributed interface LOSE the attribute since it ONLY applies to interfaces.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Interface, Inherited = false, AllowMultiple = false)]
+    public sealed class ServiceDefinitionAttribute : Attribute
+    {
+        public ServiceDefinitionAttribute()
+        {
+        }
+    }
+}
---
+++ source/R5T.Stratford/Code/Interfaces/IServiceDefinition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.Stratford
+{
+    /// <summary>
+    /// An empty interface indicating that a derived interface is a service definition.
+    /// Note: any interface that derives from an interface that derives from <see cref="IServiceDefinition"/> will ALSO derive from <see cref="IServiceDefinition"/>.
+    /// </summary>
+    public interface IServiceDefinition
+    {
+    }
+}
